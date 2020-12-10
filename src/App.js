b61ad4ee7eb56ee2fece357e6186aba7a3f57108
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-10 11:57:52 +0530

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
+import React from 'react';
 import './App.css';
 
+const api={
+  key:"e356fa72d1eddafd60fab8f755efc020",
+  url:"https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
+  const [query,setQuery] = React.useState('');
+  const [weather,setWeather] = React.useState('');
+
+  const search = evt =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(resp=>resp.json())
+      .then(result=>{setWeather(result);setQuery('');console.log(result)});
+    }
+  }
+
+
+  const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+
+    return `${day},${date} ${month}, ${year}`
+  }
+
   return (
-    <div className="app cold">
+    <div className="app">
       <main>
         <div className="search-bar">
-          <input className="search-box" type="text" placeholder="Search city ...">
+          <input className="search-box" type="text" placeholder="Search city ..."
+          onChange={e=>setQuery(e.target.value)} value={query}
+          onKeyPress={search}>
           </input>
         </div>
 
-        <div className="location-box">
-          <h2 className="location">New York City, US</h2>
-          <p className="date">Wednesay, 9th December, 2020</p>
-          <div className="temp">15 °C</div>
-          <p className="type">Sunny</p>
-        </div>
+        {(typeof weather.main != "undefined")?(
+          <div>
+            <div className="location-box">
+              <h2 className="location">{weather.name}, {weather.sys.country}</h2>
+              <p className="date">{dateBuilder(new Date())}</p>
+              <div className="temp">{weather.main.temp}°C</div>
+              <p className="type">{weather.weather[0].main}</p>
+            </div>
+          </div>
+        ):('')}
 
         <div className="copyright">&copy; Debjit Pramanick 2020, Weather App</div>
       </main>
